18d9ecc9449a4e378e223f0d2fd4c12f02956b37
Yaroslav Aleksin <yaroslavaleksin@gmail.com>
2026-08-06 23:57:34 +0300

    delete

--- sprint1/problems/final_task/solution/src/http_server.cpp
+++
@@ -1,9 +0,0 @@
-#include "http_server.h"
-
-namespace http_server {
-
-void ReportError(beast::error_code ec, std::string_view what) {
-    std::cerr << what << ": "sv << ec.message() << std::endl;
-}
-
-}
