884806972182394b778a3dc8ff23d4e07a3d65d1
Yaroslav Aleksin <yaroslavaleksin@gmail.com>
2026-08-18 02:30:05 +0300

    fix

--- sprint2/problems/server_logging/solution/src/logger.cpp
+++ sprint2/problems/server_logging/solution/src/logger.cpp
@@ -5,6 +5,7 @@
 #include <boost/log/core.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/trivial.hpp>
+#include <boost/log/utility/manipulators/add_value.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/console.hpp>
 #include <iostream>
